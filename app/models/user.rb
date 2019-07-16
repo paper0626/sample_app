89d0321344822fbb51f2a8c59a0ab80d3c9c2d4a
paper0626 <paper0626@gmail.com>
2019-07-16 09:48:28 +0000

    Add password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
   
   # saveする直前に、emailの文字列を小文字に変換する
   before_save :downcase_email
@@ -74,6 +74,22 @@ class User < ApplicationRecord
     UserMailer.account_activation(self).deliver_now
   end
   
+  # パスワードリセット用のダイジェストを生成
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest,  User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+  
+  # パスワードリセットメールを送信
+  def send_password_reset_email
+    UserMailer::password_reset(self).deliver_now
+  end
+  
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
+  
 private
 
   def downcase_email
@@ -86,5 +102,6 @@ private
     self.activation_token = User.new_token
     self.activation_digest = User.digest(activation_token)
   end
+  
 end
 
